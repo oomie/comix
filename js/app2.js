ee630befa7b3a6bf3be8a3efe85acf499c7ca976
batoxpr <bb3183@nyu.edu>
2021-10-12 21:10:16 +0400

    big change

---
+++ js/app2.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $("img:first-child").click(function(){
+        // Change src attribute of image
+        $(this).attr("src", "images/ferrari1.png");
+    });
+});
+
+$(document).ready(function(){
+    $("img:nth-child(2)").click(function(){
+        // Change src attribute of image
+        $(this).attr("src", "images/ferrari2.png");
+    });
+});
+
+$(document).ready(function(){
+    $("img:nth-child(3)").click(function(){
+        // Change src attribute of image
+        $(this).attr("src", "images/ferrari3.png");
+    });
+});
+
+$(document).ready(function(){
+    $("img:nth-child(4)").click(function(){
+        // Change src attribute of image
+        $(this).attr("src", "images/ferrari4.png");
+    });
+});
+
+
+  AOS.init();
