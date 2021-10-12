481a786159a7b1e54d342145757776ecc98f53e9
batoxpr <bb3183@nyu.edu>
2021-10-12 23:56:46 +0400

    fixed the selector

--- js/app2.js
+++ js/app2.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
-    $("img:nth-child(1)").click(function(){
+    $("img:eq(0)").click(function(){
         $(this).attr("src", "images/ferrari1.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:nth-child(2)").click(function(){
+    $("img:eq(1)").click(function(){
         $(this).attr("src", "images/ferrari2.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:nth-child(3)").click(function(){
+    $("img:eq(2)").click(function(){
         $(this).attr("src", "images/ferrari3.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:last-child").click(function(){
+    $("img:eq(3)").click(function(){
         $(this).attr("src", "images/ferrari4.png");
     });
 });
-
 
 AOS.init();
--- js/app3.js
+++ js/app3.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-    $("img:first-child").click(function(){
+    $("img:eq(0)").click(function(){
         // Change src attribute of image
         $(this).attr("src", "images/louvre1.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:nth-child(2)").click(function(){
+    $("img:eq(1)").click(function(){
         $(this).attr("src", "images/louvre2.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:nth-child(3)").click(function(){
+    $("img:eq(2)").click(function(){
         // Change src attribute of image
         $(this).attr("src", "images/louvre3.png");
     });
 });
 
 $(document).ready(function(){
-    $("img:nth-child(4)").click(function(){
+    $("img:eq(3)").click(function(){
         // Change src attribute of image
         $(this).attr("src", "images/louvre4.png");
     });
